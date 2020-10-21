b3d2d2ac8ba72d1a4c49ba2adda6d6aee013ee2e
Vaidik Kamde <vaidik16206138@gmail.com>
2020-10-21 22:07:34 +0530

    Footer Errors Resolve 7

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
-import Footer from "./footer"
 const Layout = (props) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -26,7 +25,6 @@ const Layout = (props) => {
       <main id="main">
       {props.children}
       </main>
-      <Footer/>
     </>
   )
 }
