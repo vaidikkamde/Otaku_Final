00744b38a0f9c1cb224f9b95fce51dcb190561a0
Vaidik Kamde <vaidik16206138@gmail.com>
2020-10-22 18:35:42 +0530

    Added Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Link} from "gatsby"
+
+function Footer(){
+    return(
+        <div></div>
+    )
+}
+
+export default Footer;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
+import Footer from "./Footer"
 const Layout = (props) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -25,6 +26,7 @@ const Layout = (props) => {
       <main id="main">
       {props.children}
       </main>
+      <Footer/>
     </>
   )
 }
